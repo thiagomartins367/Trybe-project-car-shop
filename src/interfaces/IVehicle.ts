5fbf4c08ff183aeb8b5ee6fcf6e6220a0335e4cf
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-29 18:19:07 -0300

    feat(IVehicle.ts): Type "IVehicle" criada com a lib "zod"

---
+++ src/interfaces/IVehicle.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+const vehicleZodSchema = z.object({
+  model: z.string(),
+  year: z.number().gte(1900).lte(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
+});
+
+export type IVehicle = z.infer<typeof vehicleZodSchema>;
+export { vehicleZodSchema };
