de339df4a024e65607221603b179b875712e21f5
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-31 20:52:45 -0300

    feat(IVehicle.ts): Validação "Zod" de quantidade miníma de caracteres implementada no atributo "model"

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 const vehicleZodSchema = z.object({
-  model: z.string(),
+  model: z.string().min(3),
   year: z.number().gte(1900).lte(2022),
   color: z.string().min(3),
   status: z.boolean().optional(),
