81498267d251ce589ef8b033e2f9e7dcee83c20e
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-31 20:49:54 -0300

    feat(CarsService.ts): Classe "CarsService" criada e método "create", para criar um novo documento, implementado

---
+++ src/services/CarsService.ts
@@ -0,0 +1,17 @@
+import { mergedSchema, ICar } from '../interfaces/ICar';
+import { IModel } from '../interfaces/IModel';
+import IService from '../interfaces/IService';
+
+class CarsService implements IService<ICar> {
+  constructor(private _carsModel: IModel<ICar>) {}
+
+  public create(body: ICar): Promise<ICar> {
+    const parsed = mergedSchema.safeParse(body);
+    if (!parsed.success) {
+      throw parsed.error;
+    }
+    return this._carsModel.create(body);
+  }
+}
+
+export default CarsService;
