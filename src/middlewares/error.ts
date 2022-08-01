66ccd778812e2bb32b785efc1ab8e52795ac5c0c
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-08-01 20:27:31 -0300

    feat(error.ts): Lógica para tratar erros catalogados implementada no Middleware de captura de erros

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,8 +1,9 @@
-import { ErrorRequestHandler, Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { ZodError } from 'zod';
+import { errorCatalog, ErrorTypes } from '../errors/catalog';
 
 export default (
-  err: ErrorRequestHandler,
+  err: Error | ZodError,
   _req: Request,
   res: Response,
   _next: NextFunction,
@@ -11,5 +12,12 @@ export default (
     return res.status(400).json({ message: err.issues });
   }
 
+  const messageAsErrorType = err.message as keyof typeof ErrorTypes;
+  const mappedError = errorCatalog[messageAsErrorType];
+  if (mappedError) {
+    const { httpStatus, error } = mappedError;
+    return res.status(httpStatus).json({ error });
+  }
+  console.error(err);
   return res.status(500).json({ message: 'Internal Server Error !' });
 };
