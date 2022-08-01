475e9ac0aae904feb4daefc2314e50a159d9309c
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-08-01 20:24:44 -0300

    feat(IModel.ts): Método "readOne" adicionado e tipado na interface "IService" e método "readAll" renomeado para "read"

--- src/interfaces/IModel.ts
+++ src/interfaces/IModel.ts
@@ -1,7 +1,7 @@
 export interface IModel<T> {
   create(obj: T): Promise<T>,
-  readAll(): Promise<T[] | []>,
-  // readOne(id: string): Promise<T | null>,
+  read(): Promise<T[] | []>,
+  readOne(_id: string): Promise<T | null>,
   // update(id: string, obj: T): Promise<T | null>,
   // delete(id: string): Promise<T | null>,
 }
