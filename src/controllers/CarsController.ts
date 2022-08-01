4ff958293d7b31b140d9ed68fda9ea9453581d92
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-08-01 20:23:00 -0300

    feat(CarsController.ts): Método "readOne", para criar listar documento de "cars" por Id criado e implementado.
    E método "readAll" renomeado para "read"

--- src/controllers/CarsController.ts
+++ src/controllers/CarsController.ts
@@ -11,8 +11,13 @@ class CarsController {
     return res.status(201).json(result);
   }
 
-  public async readAll(_req: Request, res: Response) {
-    const result = await this._service.readAll();
+  public async read(_req: Request, res: Response) {
+    const result = await this._service.read();
+    return res.status(200).json(result);
+  }
+
+  public async readOne(req: Request, res: Response) {
+    const result = await this._service.readOne(req.params.id);
     return res.status(200).json(result);
   }
 }
