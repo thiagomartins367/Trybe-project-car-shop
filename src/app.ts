3d9e0fc24de9ea53e299043c91d4b8e93d5081ea
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-07-31 21:01:46 -0300

    feat(Conversor do body das requisições para JSON, rotas da aplicação e middleware de captura de erro implementados):

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import 'express-async-errors';
+import routes from './routes';
+import errorMiddleware from './middlewares/error';
 
 const app = express();
+app.use(express.json());
+app.use(routes);
+app.use(errorMiddleware);
 
 export default app;
