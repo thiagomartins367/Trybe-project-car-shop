f56e2fe232c9aac7e313a088be8ca213fea5e01e
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-08-01 21:34:16 -0300

    feat(CarsController.ts): Método "update", para editar um documento de "cars" por Id, criado e implementado

--- src/controllers/CarsController.ts
+++ src/controllers/CarsController.ts
@@ -20,6 +20,12 @@ class CarsController {
     const result = await this._service.readOne(req.params.id);
     return res.status(200).json(result);
   }
+
+  public async update(req: Request, res: Response) {
+    const { body }: { body: ICar } = req;
+    const result = await this._service.update(req.params.id, body);
+    return res.status(200).json(result);
+  }
 }
 
 export default CarsController;
