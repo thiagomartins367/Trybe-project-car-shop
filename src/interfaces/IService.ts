6da96d7fafed25a3612993d2f8dd5c1d7d65939e
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-08-01 21:34:55 -0300

    feat(IService.ts): Método "update" adicionado e tipado na interface "IService"

--- src/interfaces/IService.ts
+++ src/interfaces/IService.ts
@@ -2,6 +2,7 @@ interface IService<T> {
   create(obj: T): Promise<T>,
   read(): Promise<T[] | []>,
   readOne(_id: string): Promise<T | null>,
+  update(id: string, obj: T): Promise<T | null>,
 }
 
 export default IService;
