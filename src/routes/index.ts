e75c0468d3cd1aa70a1dc653bb1ceda4d44c2aa1
THIAGO_MARTINS-Linux_Ubuntu <thiago17thiago@gmail.com>
2022-08-03 16:39:46 -0300

    feat(routes/index.ts): Rota "/motorcycles" adicionada ao arquivo centralizador de rotas da aplicação

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import carsRoutes from './carsRoutes';
+import motorcyclesRoutes from './motorcyclesRoutes';
 
 const routes = Router();
 
 routes.use('/cars', carsRoutes);
+routes.use('/motorcycles', motorcyclesRoutes);
 
 export default routes;
